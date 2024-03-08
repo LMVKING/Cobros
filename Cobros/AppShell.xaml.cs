6a2d2c549cdb5da8aeb787bd760bb7458d338778
lmvking <luistaveras74@gmail.com>
2024-03-08 17:37:03 -0400

    Agregar archivos de proyecto.

---
+++ Cobros/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Cobros
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Cobros/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Cobros
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
